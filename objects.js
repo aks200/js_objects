78296013a6e9671debffb173dc5c295ae549b23e
aks200 <akskn200@gmail.com>
2018-02-06 10:46:37 +0530

    object methods

---
+++ objects.js
@@ -0,0 +1,21 @@
+function fun1(){
+    var person={
+                firstname:"Aks",
+                lastname:"Kann",
+                Age:17,
+                Address:"Ahmedabad",
+
+            
+             /*fullnm:function(){
+                return this.firstname+this.lastname;
+            }*/
+                
+            };
+            person.fullnm = function(){
+                return this.Age;
+            }
+
+   
+    //document.getElementById("demo").innerHTML=person.fullnm();
+    document.getElementById("demo").innerHTML=" Age is " + person.fullnm();
+}
